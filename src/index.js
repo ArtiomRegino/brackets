d9e318197548ee069b86f97830a7f1f9e482f990
Artiom_Regino <artiom_regino@epam.com>
2021-03-12 21:54:47 +0300

    feat: add brackets implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let flat = '(';
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let zeroElementPrefix = isNaN(+ bracketsConfig[i][0]) ? '\\' : '';
+    let firstElementPrefix = isNaN(+ bracketsConfig[i][1]) ? '\\' : '';
+
+    flat += `${flat.length > 1 ? '|' : ''}${zeroElementPrefix}${bracketsConfig[i][0]}${firstElementPrefix}${bracketsConfig[i][1]}`;
+  }
+  flat += ')';
+
+  return isValid(str, flat);
+}
+
+function isValid(str, regex) {
+  let newStr = str.replace(new RegExp(regex, 'g'), '');
+
+  if(newStr.length == 0) {
+    return true;
+  }
+  
+  if(str.length == newStr.length) {
+    return false;
+  }
+
+  return isValid(newStr, regex);
 }
